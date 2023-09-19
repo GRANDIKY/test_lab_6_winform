af6c2d6db102790c48ca4dea7879cf9843731c38
GRANDIKY <mukaunitaz@gmail.com>
2023-09-19 08:57:06 +0300

    Добавьте файлы проекта.

---
+++ test_lab_6_winform/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace test_lab_6_winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.inputButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(31, 22);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(155, 13);
+            this.infoLabel.TabIndex = 1;
+            this.infoLabel.Text = "Введите поочерёдно 5 чисел ";
+            this.infoLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(72, 92);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(75, 23);
+            this.inputButton.TabIndex = 2;
+            this.inputButton.Text = "Ввод";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(126, 59);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(62, 13);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "Результат:";
+            this.resultLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(20, 56);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 20);
+            this.textBox.TabIndex = 4;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 150);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.inputButton);
+            this.Controls.Add(this.infoLabel);
+            this.Name = "Form1";
+            this.Text = "SumDigit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ test_lab_6_winform/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_lab_6_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private int quantity = 0;
+        private int result = 0;
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox.Text) & quantity < 5)
+            {
+                result += SumDigit(int.Parse(textBox.Text));
+                textBox.Text = null;
+                quantity++;
+            }
+            if (quantity == 5)
+            {
+                resultLabel.Text = "Результат: " + result.ToString();
+                quantity = 0;
+                result = 0;
+            }
+        }
+
+        private static int SumDigit(int N)
+        {
+            int S = 0;
+            while (N > 0)
+            {
+                S += N % 10;
+                N /= 10;
+            }
+            return S;
+        }
+
+        
+    }
+}
